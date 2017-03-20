4e623ec5043d5e4e6b482d282ddc47aa3d60e497
Troels Thomsen <tt@ttsoftware.net>
2017-03-20 21:57:03 +0100

    small adjustments

--- d3/src/plot/ScatterPlot.js
+++ d3/src/plot/ScatterPlot.js
@@ -39,16 +39,37 @@ export class ScatterPlot extends Plot {
         let instance = this;
 
         // Append the plot to the svg
-        svg.selectAll("dot")
+        let dots = svg.selectAll("g.dot")
             .data(this.dataPoints)
             .enter()
-            .append("circle")
-            .attr("r", 3)
+            .append("g");
+
+        // Add the dots
+        dots.append("circle")
+            .attr("class", "dot")
+            .attr("r", function (dataPoint) {
+                return Math.log(dataPoint.y);
+            })
             .attr("cx", function(dataPoint) {
                 return instance.xAxis(dataPoint.x);
             })
             .attr("cy", function(dataPoint) {
                 return instance.yAxis(dataPoint.y);
+            });
+
+        // Add the labels at (x, y)
+        dots.append("text")
+            .attr("class", "text")
+            .attr("x", function(dataPoint) {
+                return instance.xAxis(dataPoint.x);
+            })
+            .attr("y", function(dataPoint) {
+                return instance.yAxis(dataPoint.y);
+            })
+            .attr("dx", ".5em")
+            .attr("dy", ".3em")
+            .text(function(dataPoint) {
+                return dataPoint.y;
             });
 
         // Add the X Axis
